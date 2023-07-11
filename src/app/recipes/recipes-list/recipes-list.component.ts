93d35b19b13c4f08d1d08327e89d8b9be8b303d3
Robin Witt <r.witt@pmc-services.de>
2023-07-11 14:36:07 +0200

    create recipe class

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import Recipe from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
-
+  recipes: Recipe[] = [];
 }
