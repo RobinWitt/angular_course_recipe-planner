3d4b9c5c1f0385a75120588fd9d7b84730a842ff
Robin Witt <r.witt@pmc-services.de>
2023-07-11 15:08:35 +0200

    add recipe list data binding

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,5 +7,16 @@ import Recipe from '../recipe.model';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Falafel',
+      'This is a falafel recipe',
+      'https://images.unsplash.com/photo-1593001872095-7d5b3868fb1d'
+    ),
+    new Recipe(
+      'Bananabread',
+      'This is a bananabread recipe',
+      'https://images.unsplash.com/photo-1675712843856-ba2cb7d33f3c'
+    ),
+  ];
 }
