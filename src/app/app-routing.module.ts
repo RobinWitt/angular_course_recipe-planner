9b6d020ed34c82b9098f30fa71a21a7a7f4e3dd6
Robin Witt <r.witt@pmc-services.de>
2023-07-14 11:59:01 +0200

    update navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,11 +17,11 @@ const routes: Routes = [
     path: 'recipe-detail',
     component: RecipeDetailComponent,
   },
-  // {
-  //   path: '',
-  //   redirectTo: 'shopping-list',
-  //   pathMatch: 'full',
-  // },
+  {
+    path: '',
+    redirectTo: 'shopping-list',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
