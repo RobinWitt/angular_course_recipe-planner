5a8fb3f88f4e7d46c306010bd65d68a1a3e6c2f3
Robin Witt <r.witt@pmc-services.de>
2023-07-14 14:42:44 +0200

    implement add-ingredient form

--- src/app/shopping/shopping-add/shopping-add.component.ts
+++ src/app/shopping/shopping-add/shopping-add.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import Ingredient from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-add',
   templateUrl: './shopping-add.component.html',
-  styleUrls: ['./shopping-add.component.css']
+  styleUrls: ['./shopping-add.component.css'],
 })
 export class ShoppingAddComponent {
+  @ViewChild('nameInput', { static: false })
+  nameInputRef: ElementRef<HTMLInputElement> = {} as ElementRef;
+  @ViewChild('amountInput', { static: false })
+  amountInputRef: ElementRef<HTMLInputElement> = {} as ElementRef;
+  @ViewChild('unitInput', { static: false })
+  unitInputRef: ElementRef<HTMLInputElement> = {} as ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  handleAddIngredient(): void {
+    const ingredientName = String(this.nameInputRef.nativeElement.value);
+    const ingredientAmount = Number(this.amountInputRef.nativeElement.value);
+    const ingredientUnit = String(this.unitInputRef.nativeElement.value);
+    const newIngredient = new Ingredient(
+      ingredientName,
+      ingredientAmount,
+      ingredientUnit
+    );
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
